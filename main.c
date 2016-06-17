c74cad200579843e727800523be15f51b9cf8a5d
Spike88888888 <shaoleizhang@yahoo.com>
2016-06-17 06:53:14 +0000

    Add files via upload
    Added signal Handler

--- main.c
+++ main.c
@@ -3,11 +3,20 @@
 
 #define TAG		"[TEST_0]"
 
+
+void signHandler(int sig)
+{
+	printf(TAG "\n\n Kill signal detected, shutting down process. \n\n");
+}
+
 int main (int argc, char *argv[])
 {
-	// Inside test_0 program
-
+	// (1) Inside test_0 program
 	printf(TAG "\n\n Hello World.\n\n");
+
+	// (2) register a signal
+	signal(SIGINT, sigHandler);
+
 
 	return 0;
 }
