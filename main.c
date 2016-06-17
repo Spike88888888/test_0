14f8838d323d4fec491e4e7dbdc617859cc1dbaf
Spike88888888 <shaoleizhang@yahoo.com>
2016-06-17 06:35:57 +0000

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAG		"[TEST_0]"
+
+int main (int argc, char *argv[])
+{
+	// Inside test_0 program
+
+	printf(TAG "\n\n Hello World.\n\n");
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
